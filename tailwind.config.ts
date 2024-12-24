6ca4c6b9c4ea8fa5a324aac27ee1f134dbc3913d
evomatic <evantrujillo30@gmail.com>
2024-12-24 23:09:28 +0900

    chore: add design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,30 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.tsx"],
+  darkMode: ["class"],
+  content: ["./src/**/*.tsx"],
   theme: {
-  	extend: {
-  		fontFamily: {
-  			sans: [
-  				'var(--font-geist-sans)',
-                    ...fontFamily.sans
-                ]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        fire: "#FF6464",
+        bug: "#C9FF84",
+        water: "#9FF3FF",
+        flying: {
+          light: "#CBE9FF",
+          DEFAULT: "#2299EE",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
